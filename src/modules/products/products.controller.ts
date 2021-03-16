8d7b12bf61fa43d56fb0ae84cd490649a80d7759
ssmg <ssmg.sg@gmail.com>
2021-03-16 10:21:24 -0600

    Improve function

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -5,24 +5,27 @@ const getProducts = async (
   req: Request,
   res: Response
 ): Promise<Response<ProductDocument[]> | undefined> => {
+  let products: ProductDocument[]
   try {
-    const products: ProductDocument[] = await Product.find()
-
-    if (products.length === 0) {
-      return res.status(204).json()
-    } else {
-      return res.status(200).json(products)
-    }
+    products = await Product.find()
   } catch (error) {
     console.log(error)
-    return res.status(500).json('oh noes!')
+    return res
+      .status(500)
+      .json({ message: 'Server error. Please try again in a moment.' })
+  }
+
+  if (products.length === 0) {
+    return res.status(204).json()
+  } else {
+    return res.status(200).json(products)
   }
 }
 
 const getProduct = async (
   req: Request,
   res: Response
-): Promise<Response<ProductDocument> | undefined> => {
+): Promise<Response<ProductDocument[]> | undefined> => {
   try {
     const { id } = req.params
     const product: ProductDocument | null = await Product.findById(id)
